10f5c70f753aa0b4ac350a673fea946d4b9445df
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-08-14 23:39:29 +0200

    Basic classification algo; first submission

--- src/kaggle_titanic/classify.clj
+++ src/kaggle_titanic/classify.clj
@@ -3,16 +3,29 @@
                                          transformers
                                          read-csv-columnar]]
             [clojure.java.io :as io]
-            [clojure.string :refer [join]]))
+            [clojure.data.csv :as csv]
+            [clojure.string :refer [join split]]))
 
 (defn classify [tree row]
-  [1 1])
+  [(first row)
+   (loop [node tree]
+     (if (not (contains? node :splitter))
+       (if (nil? node)
+         (rand-int 2)
+         (key (apply max-key val node)))
+       (recur (get (:children node) 
+                   (nth row (dec (:splitter node)))))))])
+
+(defn transform [l]
+  (mapv #(when (> (count %2) 0) (%1 %2)) 
+        (subvec transformers 1) 
+        l))
 
 (def t (build-tree #{2 4 5 10 11}
                    (read-csv-columnar "resources/train.csv")))
 
 (with-open [i (io/reader "resources/test.csv")
             o (io/writer "submission.csv")]
-  (doseq [l (line-seq i)]
-    (.write o (str (join "," (classify t l)) "\n"))))
+  (doseq [l (csv/read-csv i)]
+    (.write o (str (join "," (classify t (transform l))) "\n"))))
 
